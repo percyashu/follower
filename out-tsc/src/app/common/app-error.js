b651aac2d59c689a1604399b89301104cbea6046
percyashu <percyayukashu@gmail.com>
2020-09-24 14:38:17 +0100

    Auto-generated commit

--- out-tsc/src/app/common/app-error.js
+++
@@ -1,8 +0,0 @@
-var AppError = /** @class */ (function () {
-    function AppError(originalError) {
-        this.originalError = originalError;
-    }
-    return AppError;
-}());
-export { AppError };
-//# sourceMappingURL=app-error.js.map
